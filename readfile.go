b90b98d989eaca9f7d7575df2d9d9dfb810e8452
igor-stefan <igorstefan@aol.com>
2022-09-25 11:23:23 -0300

    removed txt files and added gitignore

--- readfile.go
+++ readfile.go
@@ -15,16 +15,16 @@ func main() {
 	}
 
 	// open the file
-	file, err := os.Open(os.Args[1])
+	inputFile, err := os.Open(os.Args[1])
 
 	// handle errors while opening
 	if err != nil {
 		log.Fatalf("Error when opening file: %s\n", err)
 	}
-	defer file.Close() // defer to close file as soon as main ends execution
+	defer inputFile.Close() // defer to close file as soon as main ends execution
 
-	fileScanner := bufio.NewScanner(file) //  constructor
-	fileScanner.Split(bufio.ScanLines)    // configure how the scanner behaves
+	fileScanner := bufio.NewScanner(inputFile) //  constructor
+	fileScanner.Split(bufio.ScanLines)         // configure how the scanner behaves
 	// read line by line
 	for fileScanner.Scan() {
 		fmt.Println(fileScanner.Text())
