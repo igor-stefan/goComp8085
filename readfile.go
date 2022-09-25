423fc40224bc56ce12eb6c04b113494939ac6605
igor-stefan <igorstefan@aol.com>
2022-09-25 11:17:50 -0300

    Added os.Args and opening file related

--- readfile.go
+++ readfile.go
@@ -8,24 +8,29 @@ import (
 )
 
 func main() {
-	// open the file
-	file, err := os.Open("test.txt")
-
-	//handle errors while opening
-	if err != nil {
-		log.Fatalf("Error when opening file: %s", err)
+	// check if it has a file to be compiled
+	if len(os.Args) < 2 {
+		fmt.Println("Missing parameter, provide file name.")
+		return
 	}
 
-	fileScanner := bufio.NewScanner(file)
-	fileScanner.Split(bufio.ScanWords) //configure how the scanner behaves
+	// open the file
+	file, err := os.Open(os.Args[1])
+
+	// handle errors while opening
+	if err != nil {
+		log.Fatalf("Error when opening file: %s\n", err)
+	}
+	defer file.Close() // defer to close file as soon as main ends execution
+
+	fileScanner := bufio.NewScanner(file) //  constructor
+	fileScanner.Split(bufio.ScanLines)    // configure how the scanner behaves
 	// read line by line
 	for fileScanner.Scan() {
 		fmt.Println(fileScanner.Text())
 	}
 	// handle first encountered error while reading
 	if err := fileScanner.Err(); err != nil {
-		log.Fatalf("Error while reading file: %s", err)
+		log.Fatalf("Error while reading file %s\n", err)
 	}
-
-	file.Close()
 }
